034c41f32859c5240f198e7c3d737ec1f4f9b3d8
Thierry Steenhoudt <amaroknt@outlook.fr>
2025-02-06 16:47:59 +0100

    Test des routes register et login

--- backend/index.js
+++ backend/index.js
@@ -37,11 +37,11 @@ app.get("/", (req, res) => {
 app.post("/register", async (req, res) => {
     const {firstName, lastName, phone, zone, password} = req.body;
     //Fields validation
-    if(!firstName || !lastName || !lastName || !phone || !zone || !password){
+    if(!firstName || !lastName || !phone || !zone || !password){
         return res.status(400).json({message: "User information is missing"});
     }
     // Check if already registered
-    const isUser = await User.findOne({phone: phone});
+    const isUser = await User.findOne({phone});
     if(isUser){
         return res.status(400).json({message: "User already exists"});
     }
@@ -64,21 +64,21 @@ app.post("/register", async (req, res) => {
 
 //Login ---- modifier
 app.post("/login", async (req, res) => {
-    const {password, firstName} = req.body;
+    const {password, phone} = req.body;
     //handle missing info
-    if(!firstName || !password){
-        const missingField = !firstName ? "First Name" : "password";
+    if(!phone || !password){
+        const missingField = !phone ? "phone" : "password";
         return res.status(400).json({message: `${missingField} is required`});
     }
     //search the user
-    const userInfo = await User.findOne({firstName: firstName});
+    const userInfo = await User.findOne({phone});
     if(!userInfo){ //user not found
         return res.status(400).json({message: "User not found"});
     }
     if(userInfo.password !== password){ //wrong passw   ord
         return res.status(400).json({message: "Wrong password"});
     } 
-    if(userInfo.firstName !== firstName){ //wrong phone
+    if(userInfo.phone !== phone){ //wrong phone
         return res.status(400).json({message: "Wrong phone"});
     }
     const user = {user: userInfo}; 
@@ -86,85 +86,60 @@ app.post("/login", async (req, res) => {
     return res.json({error: false, message: "User logged in successfully", user, accessToken});
 });
 
-// --------------------------------- USER ROUTES ---------------------------------
+// // --------------------------------- USER ROUTES ---------------------------------
 
-//Get User Info : Profile --- modifier
-app.get("/self-info", authenticateToken, async (req, res) => {
-    const {user} = req.user;
-
-
-
-    const isUser = await User.findOne({phone: user.phone});
-    if(!isUser){
-        return res.status(400).json({message: "User not found"});
-    }
-    return res.json({error: false, user: isUser, "_id":isUser._id ,message: "User found"});
-})
+// //Get User Info : Profile --- modifier
+// app.get("/self-info", authenticateToken, async (req, res) => {
+//     const {user} = req.user;
 
 
-//--------------------------------- SWITCH REQUEST ROUTES ----------------------------------------
 
-// create request  ---  modifier
-app.post("/create-request", authenticateToken, async (req, res) => {
-    const {zone, targetUserPhone, isUrgent, startDate, endDate, userPhone} = req.body;
-    if(!zone || !startDate || !endDate || !userPhone){
-        return res.status(400).json({message: "Request information is missing"});
-    }
-    try {
-        const userInfo = await User.findOne({phone: userPhone});
-        if(!userInfo){
-            return res.status(400).json({message: "User not found"});
-        }
-        const targetUserInfo = await User.findOne({phone: targetUserPhone});
-        if(!targetUserInfo){
-            return res.status(400).json({message: "Target User not found"});
-        }
-        const request = new Request({
-            userPhone,
-            zone,
-            isUrgent: isUrgent?true:false,
-            startDate,
-            endDate,
-        });
-        await request.save();
-        return res.json({error: false, message: "Request created successfully", request: { user:userInfo, targetUser: targetUserInfo, zone, startDate, endDate, isUrgent, userShifts: userInfo.shifts}});  
-    } catch (error) {
-        return res.status(400).json({error: true, message: "Error creating request", error});
-    } 
-});
+//     const isUser = await User.findOne({phone: user.phone});
+//     if(!isUser){
+//         return res.status(400).json({message: "User not found"});
+//     }
+//     return res.json({error: false, user: isUser, "_id":isUser._id ,message: "User found"});
+// })
+
+
+// //--------------------------------- SWITCH REQUEST ROUTES ----------------------------------------
+
+// // create request  ---  modifier
+// app.post("/create-request", authenticateToken, async (req, res) => {
+//     const {zone, targetUserPhone, isUrgent, startDate, endDate, userPhone} = req.body;
+//     if(!zone || !startDate || !endDate || !userPhone){
+//         return res.status(400).json({message: "Request information is missing"});
+//     }
+//     try {
+//         const userInfo = await User.findOne({phone: userPhone});
+//         if(!userInfo){
+//             return res.status(400).json({message: "User not found"});
+//         }
+//         const targetUserInfo = await User.findOne({phone: targetUserPhone});
+//         if(!targetUserInfo){
+//             return res.status(400).json({message: "Target User not found"});
+//         }
+//         const request = new Request({
+//             userPhone,
+//             zone,
+//             isUrgent: isUrgent?true:false,
+//             startDate,
+//             endDate,
+//         });
+//         await request.save();
+//         return res.json({error: false, message: "Request created successfully", request: { user:userInfo, targetUser: targetUserInfo, zone, startDate, endDate, isUrgent, userShifts: userInfo.shifts}});  
+//     } catch (error) {
+//         return res.status(400).json({error: true, message: "Error creating request", error});
+//     } 
+// });
 
 // accept request --- modifier
-app.put("/accept-request/:requestId", authenticateToken, async (req, res) => {
-    /* const {requestId} = req.params;
-    const {accept} = req.body;
-    if(!requestId){
-        return res.status(400).json({message: "Request ID is missing"});
-    }
-    if(accept === undefined){
-        return res.status(400).json({message: "Accept field is missing"});
-    }
-    try {
-        const request = await Request.findOne({_id: requestId});
-        if(request.pending === false){
-            return res.status(400).json({message: "Request already accepted"});
-        }
-        if(!request){
-            return res.status(400).json({message: "Request not found"});
-        }
-        if(accept){
-            request.pending = false;
-            await request.save();
-            request.user
-            return res.json({error: false, message: "Request declined successfully", request});
-        }
-        return res.json({error: false, message: "Request accepted successfully", request});
-    } catch (error) {
-        return res.status(400).json({error: true, message: "Error accepting request", error});
-    } */
-});
+// app.put("/accept-request/:requestId", authenticateToken, async (req, res) => {
+   
+// });
 
-//POST      /register                                |   Créer un compte -- 
-//POST      /login                                   |   Se connecter --
+//POST      /register                                |   Créer un compte ---
+//POST      /login                                   |   Se connecter ---
 //GET       /self-info                               |   Avoir ses infos à soi --
 //POST      /request                                 |   Créer une requête de switch 
 //PUT       /request/:requestID                      |   Accepter une requête de switch
@@ -172,13 +147,13 @@ app.put("/accept-request/:requestId", authenticateToken, async (req, res) => {
 //DELETE    /delete-request                          |   Efface la requete par l'user ou accepté/refusé par l'admin
 //GET       /user/:id                                |   Avoir les info sur le user avec l'id, envoyer les requêtes --
 //GET       /request/:requestID                      |   Avoir les info sur la requete (notifications/page admin) --
-//GET       /requests-by-user/:userID                |   avoir les requêtes par user (multiple notifications)
-//GET       /schedule-user/:userId                   |   Avoir les semaines de travail par user   
-//GET       /schedule-zone/:zoneID                   |   recevoir l'horaire de la zone (par semaines)
+//GET       /requests-by-user/:userID                |   avoir les requêtes par user (multiple notifications) -- 
+//GET       /schedule-user/:userId                   |   Avoir les semaines de travail par user --
+//GET       /schedule-zone/:zoneID                   |   recevoir l'horaire de la zone (par semaines) --
 //PUT       /switch-shifts                           |   change dans les weeks la personne qui va travailler un/des jour(s)
-//GET       /admin/get-all-requests            admin |   Recevoir l'historique des requêtes
+//GET       /admin/get-all-requests            admin |   Recevoir l'historique des requêtes -- 
 //POST      /admin/create-calendar             admin |   créer calendrier avec X users aléatoires
-//GET       /admin/get-all-users               admin |   admin page; liste des users avec des info/statistiques (à voir)
+//GET       /admin/get-all-users               admin |   admin page; liste des users avec des info/statistiques (à voir) -- 
 
 app.listen(8000, () => {
     console.log("Server is running on port 8000");
