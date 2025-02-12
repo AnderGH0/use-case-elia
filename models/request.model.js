9c44d3747a1bd100e5073d4cb31f4fd4d9b430c9
Thierry Steenhoudt <amaroknt@outlook.fr>
2025-02-12 14:05:14 +0100

    modifs model request

--- models/request.model.js
+++ models/request.model.js
@@ -2,14 +2,19 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const requestSchema = new Schema({
-    serviceCenter: { type: String, required : true},
-    userPhone : { type: Number, required: true },
-    targetPhone : Number,
+    serviceCenter: { type: String, required: true },
+    userPhone: { type: Number, required: true },
+    targetPhone: Number,
     days: { type: [Date], required: true },
-    pending: { type: Boolean, default:true },   // montrer dans les notifications/requestUser
-    picked: { type: Boolean, default:false },   // quelqu'un a accepté
-    isGlobal: { type: Boolean, default:false },
-    isUrgent: { type: Boolean, default:false },
+    pending: { type: Boolean, default: true },   // montre dans les notifications/requestUser
+    picked: { type: Boolean, default: false },     // quelqu'un a accepté
+    isGlobal: { type: Boolean, default: false },
+    isUrgent: { type: Boolean, default: false },
+    // Champs ajoutés pour le switch de shift :
+    planningId: { type: mongoose.Schema.Types.ObjectId, ref: "Planning" },
+    weekId: { type: mongoose.Schema.Types.ObjectId, ref: "Week" },
+    day: { type: String },                         // ex: "monday", "tuesday", etc.
+    userFrom: { type: mongoose.Schema.Types.ObjectId, ref: "User" }
 });
 
 module.exports = mongoose.model("Request", requestSchema);
